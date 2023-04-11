ca9f83fa3b02f1362b0bae354bbf546656349b6b
Maria Korzhova <mari.shaidullova@bk.ru>
2023-04-12 00:39:04 +0300

    Task 1

---
+++ Task_5_1.java
@@ -0,0 +1,28 @@
+
+
+
+import java.util.*;
+import java.util.Map;
+
+public class Task_5_1 {
+
+public static void main(String[] args) {
+    List<String> person1 = new ArrayList<>();
+    person1.add("89999999999");
+    person1.add("89997777777");
+    List<String> person2 = new ArrayList<>();
+    person2.add("80000000000");
+    person2.add("85555555555");
+    List<String> person3 = new ArrayList<>();
+    person3.add("81111111111");
+    person3.add("82222222222");
+
+    HashMap<String, List> map = new HashMap<>(Map.of(
+        "Иван Иванов", person1, 
+        "Александр Петров", person2, 
+        "Виктор Долголюбов", person3
+    ));
+System.out.println(map);
+}
+}
+
